85a6a582a26caf1c24f59d381f6b3fa7d50f5bb5
German Bilalov <pinncho@gmail.com>
2026-04-26 12:48:42 +0200

    fix: update startupImage structure for iOS splash screen compatibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,35 +22,34 @@ export const metadata: Metadata = {
     capable: true,
     statusBarStyle: "black-translucent",
     title: "FitnessLabs",
-    startupImage: "/apple-touch-icon.png",
+    startupImage: [
+      {
+        url: "/splash/iphone16promax.png",
+        media:
+          "(device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      {
+        url: "/splash/iphone16pro.png",
+        media:
+          "(device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      {
+        url: "/splash/iphone16.png",
+        media:
+          "(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      {
+        url: "/splash/iphone15promax.png",
+        media:
+          "(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      {
+        url: "/splash/iphonese.png",
+        media:
+          "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)",
+      },
+    ],
   },
-  startupImage: [
-    {
-      url: "/splash/iphone16promax.png",
-      media:
-        "(device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3)",
-    },
-    {
-      url: "/splash/iphone16pro.png",
-      media:
-        "(device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3)",
-    },
-    {
-      url: "/splash/iphone16.png",
-      media:
-        "(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)",
-    },
-    {
-      url: "/splash/iphone15promax.png",
-      media:
-        "(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)",
-    },
-    {
-      url: "/splash/iphonese.png",
-      media:
-        "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)",
-    },
-  ],
 };
 
 export const viewport = {
